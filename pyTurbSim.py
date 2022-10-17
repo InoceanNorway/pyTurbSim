2a27200ce2e0cc072b53364d239a6929b4b7db93
hsb <hsb@inocean.no>
2022-10-17 11:54:23 +0200

    mp

--- pyTurbSim.py
+++ pyTurbSim.py
@@ -9,14 +9,23 @@ from pyts.runInput.main import readInput, run, write
 import time
 
 
-if len(sys.argv) > 1:
-    fname = sys.argv[1]
-else:
-    fname = 'Turbsim.inp'
+def runTurbsim(fname):
 
-config = readInput(fname)
 
-tm0 = time.time()
-tsdat = run(config)
-write(tsdat, config, fname)
-print 'TurbSim exited normally, runtime was %g seconds' % (time.time() - tm0)
+    config = readInput(fname)
+
+    tm0 = time.time()
+    tsdat = run(config)
+    write(tsdat, config, fname)
+    print 'TurbSim exited normally, runtime was %g seconds' % (time.time() - tm0)
+fnames=['Turbsim9.inp','Turbsim11.inp' ,'Turbsim13.inp' ,'Turbsim25.inp' ]
+
+
+if __name__=='__main__':
+    import multiprocessing as mp
+    pool=mp.Pool(4)
+    pool.map(runTurbsim,fnames)
+    pool.join()
+    pool.close()
+
+    runTurbsim(fnames[0])
